b9cc5ee9f093e089347381c411a9520213acfdae
Zoltan Kochan <zoltan.kochan@gmail.com>
2015-08-19 22:29:02 +0300

    Child processes using parent's stdios. Commands execution made synchronous.

--- index.js
+++ index.js
@@ -4,25 +4,12 @@ var chalk = require('chalk');
 var spawn = require('cross-spawn');
 var parse = require('parse-spawn-args').parse;
 
-function executeCommand(cmd, args, cwd, cb) {
+function executeCommand(cmd, args, cwd) {
   console.log(chalk.yellow('Executing: ') + cmd + ' ' + args.join(' '));
 
-  var child = spawn(cmd, args, {
-    cwd: cwd
-  });
-  var result = '';
-
-  child.stdout.on('data', function(buffer) {
-    console.log(chalk.grey(buffer.toString()));
-    result += buffer.toString();
-  });
-
-  child.stderr.on('data', function(buffer) {
-    console.log(chalk.red(buffer.toString()));
-  });
-
-  child.stdout.on('end', function() {
-    cb(result);
+  spawn.sync(cmd, args, {
+    cwd: cwd,
+    stdio: 'inherit'
   });
 }
 
@@ -30,27 +17,22 @@ function executeCommand(cmd, args, cwd, cb) {
  * @param {Array<string>} cmds - An array of commands.
  * @param {String} cwd - The directory in which the commands
  *   will be executed.
- * @param {Function} [cb] - A callback to call once the commands were executed.
  * @example
  * var cwd =  path.resolve(process.cwd());
- * exec(['mkdir foo', 'rm -rf foo']);
+ * exec(['mkdir foo', 'rm -rf foo'], cwd);
  */
-function exec(cmds, cwd, cb) {
+function exec(cmds, cwd) {
   if (cmds && cmds.length) {
     var cmd = cmds.shift();
     var parts = parse(cmd);
     if (cmd.length > 0) {
-      executeCommand(parts[0], parts.splice(1), cwd, function() {
-        console.log(chalk.yellow('Finished: ') + cmd);
-        exec(cmds, cwd, cb);
-      });
+      executeCommand(parts[0], parts.splice(1), cwd);
+      console.log(chalk.yellow('Finished: ') + cmd);
+      exec(cmds, cwd);
     }
-  } else {
-    console.log(chalk.cyan('Finished executing the commands'));
-    if (typeof cb === 'function') {
-      cb();
-    }
+    return;
   }
+  console.log(chalk.cyan('Finished executing the commands'));
 }
 
 module.exports = exec;
